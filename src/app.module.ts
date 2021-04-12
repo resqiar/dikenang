a0a47501d05915cd97ad71c1fd2244e9b0e29993
resqiar <resqiageng@gmail.com>
2021-04-12 13:17:29 +0700

    user-module-pg: change custom graphQL error messages object

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { DatabaseModule } from './database/database.module'
 import { UsersModule } from './users/users.module'
 import { join } from 'path'
+import { GraphQLError, GraphQLFormattedError } from 'graphql'
 
 @Module({
 	imports: [
@@ -10,6 +11,21 @@ import { join } from 'path'
 		UsersModule,
 		GraphQLModule.forRoot({
 			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+			/**
+			 * Intercept graphql error, bind a custom json object
+			 * @Example BadRequestException | UnauthorizedException | InvalidRequestException | etc
+			 * @returns custom message object for better error handling
+			 */
+			formatError: (error: GraphQLError) => {
+				const graphQLFormattedError: GraphQLFormattedError = {
+					message: error.message,
+					extensions: {
+						error: error.extensions.exception?.response?.error,
+						status: error.extensions.exception?.status,
+					},
+				}
+				return graphQLFormattedError
+			},
 		}),
 	],
 	controllers: [],
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql'
 import { UsersService } from './users.service'
 import { User } from './entities/user.entity'
 import { CreateUserInput } from './dto/create-user.input'
+import { BadRequestException, NotFoundException } from '@nestjs/common'
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -11,7 +12,11 @@ export class UsersResolver {
 	async createUser(
 		@Args('createUserInput') createUserInput: CreateUserInput
 	): Promise<User> {
-		return await this.usersService.create(createUserInput)
+		try {
+			return await this.usersService.create(createUserInput)
+		} catch (e) {
+			throw new BadRequestException(e.message)
+		}
 	}
 
 	@Query(() => [User], { name: 'users' })
@@ -21,6 +26,14 @@ export class UsersResolver {
 
 	@Query(() => User, { name: 'user' })
 	async findOne(@Args('id') id: string): Promise<User> {
-		return await this.usersService.findOne(id)
+		try {
+			return await this.usersService.findOne(id)
+		} catch (e) {
+			/**
+			 * @Error here means that client fails to get
+			 * correct data from the database/data not found
+			 */
+			throw new NotFoundException(e.message)
+		}
 	}
 }
