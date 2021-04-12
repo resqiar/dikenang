e0a9a8b990f9af7db69ad9bda6be98162256cc74
resqiar <resqiageng@gmail.com>
2021-04-12 11:44:33 +0700

    user-module-pg: GraphQL configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common'
-import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql'
+import { DatabaseModule } from './database/database.module'
+import { UsersModule } from './users/users.module'
+import { join } from 'path'
 
 @Module({
-	imports: [DatabaseModule],
+	imports: [
+		DatabaseModule,
+		UsersModule,
+		GraphQLModule.forRoot({
+			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+		}),
+	],
 	controllers: [],
 	providers: [],
 })
