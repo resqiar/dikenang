fa7939cb8535dc5c4c97e33e1ec80c90f62dcac6
resqiar <resqiageng@gmail.com>
2021-04-12 12:10:47 +0700

    user-module-pg: configuring user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,38 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int } from '@nestjs/graphql'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @ObjectType()
+@Entity()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+	@Field()
+	@PrimaryGeneratedColumn('uuid')
+	id: string
+
+	@Field()
+	@Column({
+		unique: true,
+	})
+	username: string
+
+	@Field()
+	@Column({
+		unique: true,
+	})
+	email: string
+
+	@Field()
+	@Column()
+	password: string
+
+	@Field(() => [String], { nullable: true })
+	@Column('text', { array: true })
+	access_token: string
+
+	@Field({ nullable: true })
+	@Column('text', { nullable: true, default: 'Hi there!' })
+	bio: string
+
+	@Field({ nullable: true })
+	@Column('text', { nullable: true })
+	avatar_url: string
 }
