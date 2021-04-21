b2c0556941f6f897e77840d9bdf45888c7d487e6
resqiar <resqiageng@gmail.com>
2021-04-21 23:57:53 +0700

    post-test: post-resolver create() unit test

--- src/posts/posts.resolver.spec.ts
+++ src/posts/posts.resolver.spec.ts
@@ -1,19 +1,67 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PostsResolver } from './posts.resolver';
-import { PostsService } from './posts.service';
+import { BadRequestException } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { PostsResolver } from './posts.resolver'
+import { PostsService } from './posts.service'
 
 describe('PostsResolver', () => {
-  let resolver: PostsResolver;
+	let resolver: PostsResolver
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsResolver, PostsService],
-    }).compile();
+	const mockPostsService = {
+		create: jest.fn(({ caption, author_id }) => {
+			/**
+			 * If caption is less than 3 chars &&
+			 * author_id is not provided throw error
+			 * normally this validation is taken care of
+			 * by class-validators, this is only for test-cases
+			 */
+			if (caption.length <= 3 || author_id.length <= 0) {
+				throw new BadRequestException()
+			}
 
-    resolver = module.get<PostsResolver>(PostsResolver);
-  });
+			return {
+				id: 'c85ea02a-2d5b-4842-90dd-9e0be3235620',
+				caption: caption,
+				author_id: author_id,
+			}
+		}),
+	}
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
-});
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [PostsResolver, PostsService],
+		})
+			.overrideProvider(PostsService)
+			.useValue(mockPostsService)
+			.compile()
+
+		resolver = module.get<PostsResolver>(PostsResolver)
+	})
+
+	it('should be defined', () => {
+		expect(resolver).toBeDefined()
+	})
+
+	it('should create a new post content', async () => {
+		const expectedResult = {
+			id: expect.any(String),
+			caption: 'testing caption',
+			author_id: 'c85ea02a-2d5b-4842-90dd-9e0be3235620',
+		}
+
+		expect(
+			await resolver.createPost({
+				caption: 'testing caption',
+				author_id: 'c85ea02a-2d5b-4842-90dd-9e0be3235620',
+			})
+		).toEqual(expectedResult)
+	})
+
+	it('should throw an error if author_id is not provided', () => {
+		expect(async () => {
+			await resolver.createPost({
+				caption: 'testing caption',
+				author_id: '',
+			})
+		}).rejects.toThrowError(new BadRequestException())
+	})
+})
