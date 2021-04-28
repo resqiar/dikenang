1b24928a0b08a27e127289df34384d1e6371107f
resqiar <resqiageng@gmail.com>
2021-04-28 21:58:58 +0700

    post-module: create-attachments input validator

--- src/posts/dto/create-attachments.input.ts
+++ src/posts/dto/create-attachments.input.ts
@@ -1,13 +1,17 @@
 import { Field, InputType } from '@nestjs/graphql'
+import { ArrayNotEmpty, IsIn } from 'class-validator'
 
 @InputType()
 export class CreateAttachmentInput {
 	@Field()
+	@IsIn(['image', 'video', 'sound'])
 	type: string
 
 	@Field()
+	@IsIn(['mp4', 'jpg', 'png', 'gif', 'mp3'])
 	extensions: string
 
 	@Field((type) => [String])
+	@ArrayNotEmpty()
 	uri: string[]
 }
