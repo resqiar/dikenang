387b2a6969046c8ee47fbf8dc16f92f209190fda
resqiar <resqiageng@gmail.com>
2021-05-07 22:41:02 +0700

    auth: register/signup unit-test block

--- src/auth/auth.resolver.spec.ts
+++ src/auth/auth.resolver.spec.ts
@@ -1,18 +1,59 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthResolver } from './auth.resolver';
+import { Test, TestingModule } from '@nestjs/testing'
+import { CreateUserInput } from '../users/dto/create-user.input'
+import { UsersService } from '../users/users.service'
+import { AuthResolver } from './auth.resolver'
+import { AuthService } from './auth.service'
 
 describe('AuthResolver', () => {
-  let resolver: AuthResolver;
+	let resolver: AuthResolver
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthResolver],
-    }).compile();
+	const mockAuthService = {}
+	const mockUsersService = {
+		create: jest.fn((createUserInput: CreateUserInput) => {
+			return {
+				id: '4500fdce-c3ff-4646-bad5-d1b7748f4b54',
+				...createUserInput,
+			}
+		}),
+	}
 
-    resolver = module.get<AuthResolver>(AuthResolver);
-  });
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				AuthResolver,
+				{
+					provide: AuthService,
+					useValue: mockAuthService,
+				},
+				{
+					provide: UsersService,
+					useValue: mockUsersService,
+				},
+			],
+		}).compile()
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
-});
+		resolver = module.get<AuthResolver>(AuthResolver)
+	})
+
+	it('should be defined', () => {
+		expect(resolver).toBeDefined()
+	})
+
+	describe('register/signup', () => {
+		it('should register a new user and return back its data', async () => {
+			const newUserInput: CreateUserInput = {
+				username: 'testing',
+				password: 'password',
+				email: 'testing@example.com',
+				avatar_url: null, //optional args
+			}
+
+			const expectedResult: Object = {
+				id: expect.any(String),
+				...newUserInput,
+			}
+
+			expect(await resolver.signUp(newUserInput)).toEqual(expectedResult)
+		})
+	})
+})
