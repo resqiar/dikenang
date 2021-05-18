5abfdcb1d568fa341eeee484ba30935b3bafcb81
resqiar <resqiageng@gmail.com>
2021-05-18 20:51:17 +0700

    chore(rollback): revert tailwind --cause limitations

--- dikweb/pages/_app.tsx
+++ dikweb/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 
 export default MyApp
