2b944b434c77bfc7449570bc28b65bbf0116d1dd
resqiar <resqiageng@gmail.com>
2021-05-19 14:53:31 +0700

    feat(dikweb): Create meta component used as meta tags

---
+++ dikweb/components/meta/Meta.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+
+type props = {
+	title?: string
+}
+
+export default function Meta({ title }: props) {
+	return (
+		<Head>
+			<title>{title ? title : 'dikenang.'}</title>
+			<meta
+				name="description"
+				content="Private social media used to share memories among linked relationship partners."
+				key="description"
+			/>
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-touch-icon.png"
+			></link>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon-32x32.png"
+			></link>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon-16x16.png"
+			></link>
+			<link rel="manifest" href="/manifest.json"></link>
+		</Head>
+	)
+}
--- dikweb/pages/index.tsx
+++ dikweb/pages/index.tsx
@@ -1,8 +1,14 @@
 import Header from '../components/header/Header'
+import Head from 'next/head'
+import Meta from '../components/meta/Meta'
 
 export default function Home() {
 	return (
 		<div>
+			{/* Default Head Meta Property */}
+			<Meta />
+
+			{/* Header Component */}
 			<Header />
 		</div>
 	)
