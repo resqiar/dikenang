0b9910a8f1ff8ae4b934d4e004be2c8f9db48faa
resqiar <resqiageng@gmail.com>
2021-05-20 23:51:01 +0700

    fix(dikweb): attempt to fix unshowed image on manifest.json [1]

--- dikweb/components/meta/Meta.tsx
+++ dikweb/components/meta/Meta.tsx
@@ -8,10 +8,43 @@ export default function Meta({ title }: props) {
 	return (
 		<Head>
 			<title>{title ? title : 'dikenang.'}</title>
+
+			{/* <!-- Primary Meta Tags --> */}
+			<meta
+				name="title"
+				content="dikenang. — Keep your memories alive, forever 🌺"
+			></meta>
 			<meta
 				name="description"
-				content="Keep your memories alive with dikenang."
-			/>
+				content="Share everything, images, videos, stories, memories, and more!"
+			></meta>
+
+			{/* <!-- Open Graph / Facebook --> */}
+			<meta property="og:type" content="website"></meta>
+			<meta property="og:url" content=""></meta>
+			<meta
+				property="og:title"
+				content="dikenang. — Keep your memories alive, forever 🌺"
+			></meta>
+			<meta
+				property="og:description"
+				content="Share everything, images, videos, stories, memories, and more!"
+			></meta>
+			<meta property="og:image" content="/dikenang-logo.png"></meta>
+
+			{/* <!-- Twitter --> */}
+			<meta property="twitter:card" content="summary_large_image"></meta>
+			<meta property="twitter:url" content=""></meta>
+			<meta
+				property="twitter:title"
+				content="dikenang. — Keep your memories alive, forever 🌺"
+			></meta>
+			<meta
+				property="twitter:description"
+				content="Share everything, images, videos, stories, memories, and more!"
+			></meta>
+			<meta property="twitter:image" content="/dikenang-logo.png"></meta>
+
 			<link
 				rel="apple-touch-icon"
 				sizes="180x180"
