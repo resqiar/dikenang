fa6eda009699921f38494b4bd2ca66125a8a1820
resqiar <resqiageng@gmail.com>
2021-05-29 13:00:14 +0700

    refactor: dikenang logo components

--- dikweb/components/logo/DikenangLogo.tsx
+++ dikweb/components/logo/DikenangLogo.tsx
@@ -1,15 +1,45 @@
-import styles from './DikenangLogo.module.css'
+import styled from 'styled-components'
 
-export default function DikenangLogo() {
+interface Props {
+	text?: string
+}
+
+export default function DikenangLogo(props: Props) {
 	/**
 	 * Dikenang Logo.
 	 * Crafted by hand
 	 */
 	return (
-		<div className={styles.logo}>
-			<h1>
-				di<span>kenang.</span>
-			</h1>
-		</div>
+		<DikenangLogoWrapper>
+			<DikenangLogoH1>
+				{props.text ? props.text : 'dikenang.'}
+			</DikenangLogoH1>
+		</DikenangLogoWrapper>
 	)
 }
+
+const DikenangLogoWrapper = styled.div`
+	width: min-content;
+	cursor: pointer;
+`
+
+const DikenangLogoH1 = styled.h1`
+	background: linear-gradient(
+		90deg,
+		rgba(255, 105, 180, 1) 15%,
+		rgba(0, 0, 255, 1) 68%
+	);
+
+	/* Set the background size and repeat properties. */
+	background-size: 100%;
+	background-position: center;
+	background-repeat: no-repeat;
+
+	/* Use the text as a mask for the background. */
+	/* This will show the gradient as a text color rather than element bg. */
+	-webkit-background-clip: text;
+	background-clip: text;
+	-webkit-text-fill-color: transparent;
+	-moz-background-clip: text;
+	-moz-text-fill-color: transparent;
+`
