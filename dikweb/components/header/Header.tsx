c5bf0ace9f1d4488688a75185bebd7688a471460
resqiar <resqiageng@gmail.com>
2021-05-29 12:42:50 +0700

    refactor: Header to styled-components

--- dikweb/components/header/Header.tsx
+++ dikweb/components/header/Header.tsx
@@ -1,7 +1,5 @@
 import Icons from '../icons/Icons'
 import DikenangLogo from '../logo/DikenangLogo'
-import styles from './Header.module.css'
-
 import {
 	SearchOutlined,
 	ChatOutlined,
@@ -10,17 +8,18 @@ import {
 } from '@material-ui/icons'
 import Input from '../input/Input'
 import { Avatar, IconButton } from '@material-ui/core'
+import styled from 'styled-components'
 
 export default function Header() {
 	return (
-		<div className={styles.header}>
+		<HeaderWrapper>
 			{/* Left Side */}
-			<div className={styles.headerBrand}>
+			<HeaderBrand>
 				{/* Brand Logo */}
 				<DikenangLogo />
 
 				{/* Search Component */}
-				<div className={styles.headerSearchInput}>
+				<HeaderSearchInput>
 					<Input
 						placeholder="Search for memories, partners, and stuff"
 						type="text"
@@ -31,25 +30,68 @@ export default function Header() {
 						// value will dismissed when onBlur
 						isDismissedOnBlur={true}
 					/>
-				</div>
-			</div>
+				</HeaderSearchInput>
+			</HeaderBrand>
 
 			{/* Right Side */}
-			<div className={styles.headerIconWrapper}>
-				<div className={styles.headerIconList}>
+			<HeaderIconsWrapper>
+				<HeaderIconsList>
 					{/* Mockup for now, future works will replace the following */}
 					<Icons Icon={FavoriteBorder} color="purple" hasIconButton />
 					<Icons Icon={ChatOutlined} hasIconButton />
 					<Icons Icon={NotificationsActiveOutlined} hasIconButton />
-				</div>
+				</HeaderIconsList>
 
-				<div className={styles.headerAvatarIconWrapper}>
+				<HeaderAvatarWrapper>
 					{/* Avatar Icon */}
 					<IconButton>
-						<Avatar className={styles.headerAvatarIcon} />
+						<Avatar />
 					</IconButton>
-				</div>
-			</div>
-		</div>
+				</HeaderAvatarWrapper>
+			</HeaderIconsWrapper>
+		</HeaderWrapper>
 	)
 }
+
+const HeaderWrapper = styled.div`
+	background: transparent;
+	padding: 2px 10%;
+	display: flex;
+	min-height: 55px;
+	align-items: center;
+	justify-content: space-between;
+
+	// how mobile should behave
+	@media (max-width: 600px) {
+		justify-content: space-between;
+		padding: 0px 12px 0px 24px;
+	}
+`
+const HeaderBrand = styled.div`
+	display: flex;
+	align-items: center;
+	flex-grow: 1;
+`
+const HeaderSearchInput = styled.div`
+	width: 100%;
+	padding: 0px 18px;
+
+	// how mobile should behave
+	@media (max-width: 600px) {
+		display: none;
+	}
+`
+const HeaderIconsWrapper = styled.div`
+	display: flex;
+	align-items: center;
+`
+const HeaderIconsList = styled.div`
+	display: flex;
+	align-items: center;
+
+	// how mobile should behave
+	@media (max-width: 600px) {
+		display: none;
+	}
+`
+const HeaderAvatarWrapper = styled.div``
