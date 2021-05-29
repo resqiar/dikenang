b161886314370b9d51ee8f6001eb8ba8af52cc92
resqiar <resqiageng@gmail.com>
2021-05-29 14:02:19 +0700

    refactor: sidebar & mid wrapper to fit styled-components

--- dikweb/components/sidebar/rightbar/Rightbar.tsx
+++ dikweb/components/sidebar/rightbar/Rightbar.tsx
@@ -1,11 +1,27 @@
+import styled from 'styled-components'
 import SidebarCard from '../container/SidebarCard'
-import styles from './Rightbar.module.css'
 
 export default function Rightbar() {
 	return (
-		<div className={styles.rightbarWrapper}>
+		<RightBarWrapper>
 			{/* Card */}
 			<SidebarCard />
-		</div>
+		</RightBarWrapper>
 	)
 }
+
+const RightBarWrapper = styled.div`
+	flex: 0.2;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	margin: 18px 8px;
+	height: min-content;
+	top: 20px;
+
+	/* How mobile should behave */
+	@media (max-width: 600px) {
+		flex: 0;
+		display: none;
+	}
+`
