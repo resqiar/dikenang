66954780faa8dd8f30f3f5f6352dfba2a697b354
resqiar <resqiageng@gmail.com>
2021-06-02 12:57:19 +0700

    fix: sidebar card component's background color

--- dikweb/components/sidebar/container/SidebarCard.tsx
+++ dikweb/components/sidebar/container/SidebarCard.tsx
@@ -1,21 +1,51 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 interface Props {
 	children: JSX.Element | JSX.Element[]
+	bgColor?: string
+	hasShadow?: boolean
+	hoverBg?: string
 }
 
-export default function SidebarCard({ children }: Props) {
-	return <SideBarCardWrapper>{children}</SideBarCardWrapper>
+export default function SidebarCard({
+	children,
+	bgColor,
+	hasShadow,
+	hoverBg,
+}: Props) {
+	return (
+		<SideBarCardWrapper
+			bgColor={bgColor}
+			hasShadow={hasShadow}
+			hoverBg={hoverBg}
+		>
+			{children}
+		</SideBarCardWrapper>
+	)
 }
 
-const SideBarCardWrapper = styled.div`
+const SideBarCardWrapper = styled.div<{
+	bgColor?: string
+	hasShadow?: boolean
+	hoverBg?: string
+}>`
 	width: 100%;
 	height: min-content;
 	text-align: center;
-	background-color: var(--background-dimmed-300);
+	background-color: ${(props) => props.bgColor || 'transparent'};
 	border-radius: 8px;
 	margin-bottom: 8px;
-	box-shadow: var(--box-shadow);
 	transition: 0.3s;
 	cursor: pointer;
+
+	/* If props has a shadow */
+	${(props) =>
+		props.hasShadow &&
+		css`
+			box-shadow: var(--box-shadow);
+		`}
+
+	&:hover {
+		background-color: ${(props) => props.hoverBg || undefined};
+	}
 `
