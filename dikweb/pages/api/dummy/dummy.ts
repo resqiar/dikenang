a7a7d42bc6c49f37b55c63846de30ad79add3829
resqiar <resqiageng@gmail.com>
2021-06-07 16:27:35 +0700

    feat(dikweb): create static mockup to simulate calling APIs

---
+++ dikweb/pages/api/dummy/dummy.ts
@@ -0,0 +1,46 @@
+export const dummy = [
+	{
+		id: 'UAdiuawbdasdyawdahwbdsbaiwu',
+		caption:
+			'It was, as far as I can ascertain, in September of the year 1811 that a post-chaise drew up before the door of Aswarby Hall, in the heart of Lincolnshire. The little boy who was the only passenger in the chaise, and who jumped out as soon as it had stopped, looked about him with the keenest curiosity during the short interval that elapsed between the ringing of the bell and the opening of the hall door. He saw a tall, square, red-brick house, built in the reign of Anne; a stone-pillared porch had been added in the purer classical style of 1790; the windows of the house were many, tall and narrow, with small panes and thick white woodwork. A pediment, pierced with a round window, crowned the front. There were wings to right and left, connected by curious glazed galleries, supported by colonnades, with the central block. These wings plainly contained the stables and offices of the house. Each was surmounted by an ornamental cupola with a gilded vane. An evening light shone on the building, making the window-panes glow like so many fires. Away from the Hall in front stretched a flat park studded with oaks and fringed with firs, which stood out against the sky. The clock in the church-tower, buried in trees on the edge of the park, only its golden weather-cock catching the light, was striking six, and the sound came gently beating down the wind. It was altogether a pleasant impression, though tinged with the sort of melancholy appropriate to an evening in early autumn, that was conveyed to the mind of the boy who was standing in the porch waiting for the door to open to him.',
+		author: 'dikenang.dev',
+		timestamp: '2 hours ago',
+		summary: {
+			upvotes: 21200,
+			downvotes: 5476,
+			comments: 19000,
+		},
+	},
+	{
+		id: 'UAdiuawbdasdyawdahwbdsbaiwadaw',
+		caption:
+			'The post-chaise had brought him from Warwickshire, where, some six months before, he had been left an orphan. Now, owing to the generous offer of his elderly cousin, Mr Abney, he had come to live at Aswarby. The offer was unexpected, because all who knew anything of Mr Abney looked upon him as a somewhat austere recluse, into whose steady-going household the advent of a small boy would import a new and, it seemed, incongruous element. The truth is that very little was known of Mr Abney’s pursuits or temper. The Professor of Greek at Cambridge had been heard to say that no one knew more of the religious beliefs of the later pagans than did the owner of Aswarby. Certainly his library contained all the then available books bearing on the Mysteries, the Orphic poems, the worship of Mithras, and the Neo–Platonists. In the marble-paved hall stood a fine group of Mithras slaying a bull, which had been imported from the Levant at great expense by the owner. He had contributed a description of it to the Gentleman’s Magazine , and he had written a remarkable series of articles in the Critical Museum on the superstitions of the Romans of the Lower Empire. He was looked upon, in fine, as a man wrapped up in his books, and it was a matter of great surprise among his neighbours that he should ever have heard of his orphan cousin, Stephen Elliott, much more that he should have volunteered to make him an inmate of Aswarby Hall.',
+		author: 'dikenang.dev',
+		timestamp: '2 hours ago',
+		summary: {
+			upvotes: 109200,
+			downvotes: 14476,
+			comments: 59000,
+		},
+		attachments: {
+			type: 'image/png',
+			path: '/images/bg.png',
+		},
+	},
+	{
+		id: 'UAdiuawbdasdyawdahwbdsbajncc',
+		caption:
+			'That night he had a curious dream. At the end of the passage at the top of the house, in which his bedroom was situated, there was an old disused bathroom. It was kept locked, but the upper half of the door was glazed, and, since the muslin curtains which used to hang there had long been gone, you could look in and see the lead-lined bath affixed to the wall on the right hand, with its head towards the window. On the night of which I am speaking, Stephen Elliott found himself, as he thought, looking through the glazed door. The moon was shining through the window, and he was gazing at a figure which lay in the bath. His description of what he saw reminds me of what I once beheld myself in the famous vaults of St Michan’s Church in Dublin, which possesses the horrid property of preserving corpses from decay for centuries. A figure inexpressibly thin and pathetic, of a dusty leaden colour, enveloped in a shroud-like garment, the thin lips crooked into a faint and dreadful smile, the hands pressed tightly over the region of the heart. As he looked upon it, a distant, almost inaudible moan seemed to issue from its lips, and the arms began to stir. The terror of the sight forced Stephen backwards and he awoke to the fact that he was indeed standing on the cold boarded floor of the passage in the full light of the moon. With a courage which I do not think can be common among boys of his age, he went to the door of the bathroom to ascertain if the figure of his dreams were really there. It was not, and he went back to bed.',
+		author: 'dikenang.dev',
+		timestamp: '2 weeks ago',
+		summary: {
+			upvotes: 1200,
+			downvotes: 15476,
+			comments: 1000,
+		},
+		attachments: {
+			type: 'image/png',
+			path: '/images/bg-3.png',
+		},
+	},
+]
