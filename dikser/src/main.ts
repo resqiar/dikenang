00293b385c8e8bbaf9673ae1c4eaaf494488155c
resqiar <resqiageng@gmail.com>
2021-06-13 20:05:49 +0700

    chore(session-dikser): update configurations

--- dikser/src/main.ts
+++ dikser/src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
 		session({
 			cookie: {
 				maxAge: 86400000, // 1 day
-				secure: true, // transmit only over https
+				secure: process.env.NODE_ENV === 'production', // transmit only over https
 				httpOnly: true, // prevent client JS reading the cookie
 			},
 			secret: process.env.SESSION_KEY!,
