015d784a1e7840984c87f4bb2c9eebd77c0eab38
resqiar <resqiageng@gmail.com>
2021-06-14 19:21:36 +0700

    feat(dikweb): enable server-side rendering to get user profile from oAuth providers

--- dikweb/pages/index.tsx
+++ dikweb/pages/index.tsx
@@ -1,34 +1,65 @@
-import { GetStaticProps } from 'next'
+import { NextPageContext } from 'next'
 import IndexBody from '../components/body/IndexBody'
 import Header from '../components/header/Header'
 import Meta from '../components/meta/Meta'
 import { DummyPostType } from '../types/dummypost.type'
 import { dummy } from './api/dummy/dummy'
+import checkAuth from '../utils/auth'
+import { UserProfileType } from '../types/profile.type'
 
 interface Props {
 	posts: DummyPostType
+	user: UserProfileType
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-	/**
-	 * Get static data from dummy
-	 * @see './api/dummy/dummy'
-	 */
-	const posts = dummy
-	return { props: { posts } }
-}
-
-export default function Home({ posts }: Props) {
+export default function Home({ posts, user }: Props) {
 	return (
 		<div>
 			{/* Default Head Meta Property */}
 			<Meta title="Dashboard — dikenang" />
 
 			{/* Header Component */}
-			<Header />
+			<Header profile={user} />
 
 			{/* Body Component */}
-			<IndexBody posts={posts} />
+			<IndexBody posts={posts} profile={user} />
 		</div>
 	)
+}
+
+export async function getServerSideProps(ctx: NextPageContext) {
+	/**
+	 * Dummmy Post just yet
+	 * @NO APi calls
+	 */
+	const posts = dummy
+	/**
+	 * Check if cookie is exist
+	 * if not => redirect to login
+	 */
+	const cookie = ctx.req?.headers.cookie
+
+	if (cookie === undefined)
+		return {
+			redirect: {
+				destination: '/auth',
+				permanent: false,
+			},
+		}
+
+	/**
+	 * Get User data profile from server
+	 * if not exist => redirect to login
+	 */
+	const data = await checkAuth(ctx)
+
+	if (!data)
+		return {
+			redirect: {
+				destination: '/auth',
+				permanent: false,
+			},
+		}
+
+	return { props: { posts: posts, user: data } }
 }
