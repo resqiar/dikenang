effb5fa3234f34dd3f8509ea5e4e989f01759379
resqiar <resqiageng@gmail.com>
2021-06-17 22:56:37 +0700

    fix(dikweb): fix dikweb `base_url` configurations

--- dikweb/utils/axios.ts
+++ dikweb/utils/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from 'axios'
 const baseURL =
 	process.env.NODE_ENV === 'development'
 		? 'http://localhost:3000/'
-		: 'http://dikenang.vercel.app'
+		: process.env.NEXT_PUBLIC_BACKEND_URI
 
 const axiosConfig: AxiosInstance = axios.create({
 	baseURL,
