18f7abb97bc50ece60808fef8b7f2e3ae793f7d3
resqiar <resqiageng@gmail.com>
2021-06-18 14:58:23 +0700

    fix(dikweb): fix bug when auth cookie somehow, lost.

--- dikweb/utils/auth.ts
+++ dikweb/utils/auth.ts
@@ -2,13 +2,12 @@ import { NextPageContext } from 'next'
 import axiosConfig from './axios'
 
 export default async function checkAuth(ctx: NextPageContext) {
-	const resp = await axiosConfig.get('/auth/status', {
-		headers: { cookie: ctx.req!.headers.cookie },
-	})
-
-	if (resp.status === 403) {
+	try {
+		const resp = await axiosConfig.get('/auth/status', {
+			headers: { cookie: ctx.req!.headers.cookie },
+		})
+		return resp.data
+	} catch (e) {
 		return
 	}
-
-	return resp.data
 }
