85bd47da7f610b89aacf41e5c0882939c9d18ad6
resqiar <resqiageng@gmail.com>
2021-07-16 15:12:47 +0700

    chore: trigger deploy

--- dikser/src/app.module.ts
+++ dikser/src/app.module.ts
@@ -18,7 +18,6 @@ import { BadgesModule } from './badges/badges.module'
 		GraphQLModule.forRoot({
 			playground: process.env.NODE_ENV !== 'production',
 			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-			// cors
 			cors: {
 				origin: [process.env.CLIENT_ORIGIN!, process.env.A_ORIGIN!],
 				credentials: true,
