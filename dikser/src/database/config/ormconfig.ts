9caf84c9e9dfcbf7fadc29511a5142f8287f0269
resqiar <resqiageng@gmail.com>
2021-07-17 22:32:37 +0700

    chore(dikser): debug ssl config

--- dikser/src/database/config/ormconfig.ts
+++ dikser/src/database/config/ormconfig.ts
@@ -3,9 +3,7 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 const config: PostgresConnectionOptions = {
 	type: 'postgres',
 	url: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false,
-	},
+	ssl: process.env.NODE_ENV === 'production' ? true : false,
 	entities: ['dist/**/*.entity{.ts,.js}'],
 
 	// synchronization should set to false when it comes to production
