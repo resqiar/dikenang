42a0c8d6b45ce64782015778f5f1d7b048645350
resqiar <resqiageng@gmail.com>
2021-08-05 00:02:06 +0700

    fix(redis): fix redis pub/sub not include "Date" in subscriptions

--- dikser/src/shared/utils/redispubsub.ts
+++ dikser/src/shared/utils/redispubsub.ts
@@ -2,6 +2,24 @@ import { RedisPubSub } from 'graphql-redis-subscriptions'
 import * as Redis from 'ioredis'
 
 export function configureRedisPubSub() {
+	/**
+	 * This function is used to "revive" Date object.
+	 * previously, Date object will vanish after entering redis pub/sub.
+	 * in other words, created_at, updated_at field will always return null.
+	 * @see https://github.com/davidyaha/graphql-redis-subscriptions#using-a-custom-reviver
+	 */
+	const dateReviver = (_key: any, value: any) => {
+		const isISO8601Z =
+			/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/
+		if (typeof value === 'string' && isISO8601Z.test(value)) {
+			const tempDateNumber = Date.parse(value)
+			if (!isNaN(tempDateNumber)) {
+				return new Date(tempDateNumber)
+			}
+		}
+		return value
+	}
+
 	return new RedisPubSub({
 		publisher: new Redis(
 			process.env.REDIS_TLS_URL,
@@ -23,5 +41,6 @@ export function configureRedisPubSub() {
 				  }
 				: undefined
 		),
+		reviver: dateReviver,
 	})
 }
