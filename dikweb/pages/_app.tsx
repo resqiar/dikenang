e69419bd0bafb7702d37826a0aca5bb9130a7831
resqiar <resqiageng@gmail.com>
2021-08-15 14:38:42 +0700

    chore: trigger vercel deploy

--- dikweb/pages/_app.tsx
+++ dikweb/pages/_app.tsx
@@ -6,6 +6,7 @@ import Router from 'next/router'
 import NProgress from 'nprogress'
 import '../styles/globals.css'
 import '../styles/nprogress.css'
+
 /**
  * Custom Override styles for RichTextEditor
  * @RichTextEditor @PrismDecorator
