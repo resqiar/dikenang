0a34905aed59cc543e3073d5845baa1edfa68c93
resqiar <resqiageng@gmail.com>
2021-09-03 17:29:25 +0700

    chore: completely remove SSR since slow load time still exist even without server call?

--- dikweb/pages/index.tsx
+++ dikweb/pages/index.tsx
@@ -1,4 +1,4 @@
-import { NextPageContext } from 'next'
+// import { NextPageContext } from 'next'
 import IndexBody from '../components/body/IndexBody'
 import Header from '../components/header/Header'
 import Meta from '../components/meta/Meta'
@@ -39,40 +39,40 @@ export default function Home({ user }: Props) {
 	)
 }
 
-export async function getServerSideProps(_ctx: NextPageContext) {
-	/**
-	 * Check if cookie is exist
-	 * if not => redirect to login page.
-	 */
-	// const cookie = ctx.req?.headers.cookie
+// export async function getServerSideProps(_ctx: NextPageContext) {
+/**
+ * Check if cookie is exist
+ * if not => redirect to login page.
+ */
+// const cookie = ctx.req?.headers.cookie
 
-	// if (cookie === undefined)
-	// 	return {
-	// 		redirect: {
-	// 			destination: '/auth',
-	// 			permanent: false,
-	// 		},
-	// 	}
+// if (cookie === undefined)
+// 	return {
+// 		redirect: {
+// 			destination: '/auth',
+// 			permanent: false,
+// 		},
+// 	}
 
-	/**
-	 * Get User data profile from server
-	 * if not exist => redirect to login page
-	 * @param pass NextPageContext in order to obtain
-	 * cookie when in server-side mode
-	 */
-	// const data = await checkAuth(ctx)
+/**
+ * Get User data profile from server
+ * if not exist => redirect to login page
+ * @param pass NextPageContext in order to obtain
+ * cookie when in server-side mode
+ */
+// const data = await checkAuth(ctx)
 
-	// if (!data)
-	return {
-		redirect: {
-			destination: '/auth',
-			permanent: false,
-		},
-	}
+// if (!data)
+// return {
+// 	redirect: {
+// 		destination: '/auth',
+// 		permanent: false,
+// 	},
+// }
 
-	// return {
-	// 	props: {
-	// 		user: data,
-	// 	},
-	// }
-}
+// return {
+// 	props: {
+// 		user: data,
+// 	},
+// }
+// }
