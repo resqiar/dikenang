bb4e812db4ceea157d40328b12ae8960d59fe2fe
resqiar <resqiageng@gmail.com>
2021-09-09 12:12:33 +0700

    test: measure performance in puppeteer

--- puppeteer/src/index.ts
+++ puppeteer/src/index.ts
@@ -1,9 +1,16 @@
 import puppeteer from 'puppeteer'
 ;(async () => {
-	const browser = await puppeteer.launch()
+	const browser = await puppeteer.launch({ headless: false })
 	const page = await browser.newPage()
 	await page.goto('https://dikenang.co', { waitUntil: 'networkidle0' })
-	await page.screenshot({ path: 'media/dikenang.jpg' })
+
+	// Executes Navigation API within the page context
+	const metrics = await page.evaluate(() =>
+		JSON.stringify(window.performance)
+	)
+
+	// Parses the result to JSON
+	console.info(JSON.parse(metrics))
 
 	await browser.close()
 })()
